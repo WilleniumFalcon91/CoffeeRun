4d9961914c55658dcdf7b53c8eb5d913a1c26959
William Elliott <wcelliott1991@gmail.com>
2017-07-21 16:16:51 -0400

    builds field specific form fields

--- scripts/main.js
+++ scripts/main.js
@@ -1,33 +1,47 @@
-var $coffeeOrderSection = $('[data-coffee-order]');
-var $coffeeOrderData =  $("#coffeeOrder");
 var URL = 'http://dc-coffeerun.herokuapp.com/api/coffeeorders';
 var FORMSELECTOR = '[data-coffee-order="form"]';
+
 var COFFEESELECTOR = '[data-coffee-order="coffee"]';
+var EMAILSELECTOR = '[data-email-input="email"]';
+var SIZESHORTSELECTOR = '[data-size-selection="short"]'
+var SIZETALLSELECTOR = '[data-size-selection="tall"]'
+var SIZEGRANDESELECTOR = '[data-size-selection="grande"]'
+var CARAMELFLAVORSHOTSELECTOR = '[data-flavor="caramel"]'
+var ALMONDFLAVORSHOTSELECTOR = '[data-flavor="almond"]'
+var MOCHAFLAVORSHOTSELECTOR = '[data-flavor="mocha"]'
+var CAFFEINERATINGSELECTOR = '[data-caffeine="rating"]'
 
 function storesValue (key, value) {
     localStorage.setItem(key, value);
-    // localStorage.getItem(key);
 }
-
-
-
-
-// $.get(URL, function (data) {
-//  console.log(data);
-// });
 
 function main (form) {
     form.on('submit', function (event){
         event.preventDefault();
-        // console.log($(COFFEESELECTOR).val());
-        var $coffeeInput = $(COFFEESELECTOR);
-        var keyName = $coffeeInput.attr('name');
-        // console.log(keyName);
-        var coffeeValue = $coffeeInput.val(); 
-        // console.log(coffeeValue);
-        storesValue(keyName, coffeeValue);
-        // storesValue($coffeeOrderSection, $coffeeOrderData);
-        // $.get(URL, storesValue);
+        extractValue(COFFEESELECTOR);
+        extractValue(EMAILSELECTOR);
+        extractValue();
+        extractValue(CAFFEINERATINGSELECTOR);
     })
+}
+
+function extractTextValue(formField) {
+    var $input = $(formField);
+    var keyName = $input.attr('name');
+    var value = $input.val(); 
+    storesValue(keyName, value);
+}
+function extractRadioValue(formField) { //look up how to get value of radio button
+    var $input = $(formField);
+    var keyName = $input.attr('name');
+    // var value = $input.val(); 
+    storesValue(keyName, value);
+} 
+
+function extractDropValue(formField) { //look up how to get value of dropdown 
+    var $input = $(formField);
+    var keyName = $input.attr('name');
+    // var value = $input.val(); 
+    storesValue(keyName, value);
 }
 main($(FORMSELECTOR));
