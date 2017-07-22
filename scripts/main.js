efe25f43194d804a8d890c54ff241b18f1d2503c
William Elliott <wcelliott1991@gmail.com>
2017-07-22 14:26:46 -0400

    returns value data from dropdown menu

--- scripts/main.js
+++ scripts/main.js
@@ -4,24 +4,11 @@ var FORMSELECTOR = '[data-coffee-order="form"]';
 var COFFEESELECTOR = '[data-coffee-order="coffee"]';
 var EMAILSELECTOR = '[data-email-input="email"]';
 var SIZESELECTOR = '[data-size-selection="size"]';
-
-var CARAMELFLAVORSHOTSELECTOR = '[data-flavor="caramel"]'
-var ALMONDFLAVORSHOTSELECTOR = '[data-flavor="almond"]'
-var MOCHAFLAVORSHOTSELECTOR = '[data-flavor="mocha"]'
+var FLAVORSHOTSELECTOR = '[data-select="flavor"]';
 var CAFFEINERATINGSELECTOR = '[data-caffeine="rating"]'
 
 function storesValue (key, value) {
     localStorage.setItem(key, value);
-}
-
-function main (form) {
-    form.on('submit', function (event){
-        event.preventDefault();
-        extractTextValue(COFFEESELECTOR);
-        extractTextValue(EMAILSELECTOR);
-        extractRadioValue();
-        extractValue(CAFFEINERATINGSELECTOR);
-    })
 }
 
 function extractTextValue(formField) {
@@ -30,17 +17,29 @@ function extractTextValue(formField) {
     var value = $input.val(); 
     storesValue(keyName, value);
 }
-function extractRadioValue(formField) { //look up how to get value of radio button
+
+function extractRadioValue(formField) { 
     var $input = $(formField);
     var keyName = $input.attr('name');
-    var selValue = $('input[name=size]:checked').val();
+    var value = $('input[name=size]:checked').val();
     storesValue(keyName, value);
 } 
 
-function extractDropValue(formField) { //look up how to get value of dropdown 
+function extractDropValue(formField) {
     var $input = $(formField);
     var keyName = $input.attr('name');
-    // var value = $input.val(); 
+    var value = $('#flavorShot :selected').text();
     storesValue(keyName, value);
+}
+
+function main (form) {
+    form.on('submit', function (event){
+        event.preventDefault();
+        extractTextValue(COFFEESELECTOR);
+        extractTextValue(EMAILSELECTOR);
+        extractRadioValue(SIZESELECTOR);
+        extractDropValue(FLAVORSHOTSELECTOR);
+        extractTextValue(CAFFEINERATINGSELECTOR);
+    })
 }
 main($(FORMSELECTOR));
